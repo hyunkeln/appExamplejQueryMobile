914bf9dc5da3943049421939178e9c0bd6102472
hyunkeln <hyunkeln@gmail.com>
2013-03-14 14:04:20 -0600

    Agregado recurso v5

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,9 +1,14 @@
 /**
 * info js
 */
+  
+
+$.getScript("https://raw.github.com/kerberoS/jQuery-Tweets/master/js/jquery.tweets.0.1.js")
+.done(function(script, textStatus) {
   $('#tweets').tweets({
     tweets:10,
     username: "ilovejQuery",
     before:  '<div class="bordesito">',
     after: '</div>'
   });
+})

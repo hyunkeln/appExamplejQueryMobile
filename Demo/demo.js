a5f6ff65191d2c74e8f69e4a337002254b8d8802
hyunkeln <hyunkeln@gmail.com>
2013-03-14 13:29:54 -0600

    Cambiado js

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,11 +1,9 @@
 /**
 * info js
 */
-window.addEvent('domready', function() {
   $('#tweets').tweets({
     tweets:10,
     username: "ilovejQuery",
     before:  '<div class="bordesito">',
     after: '</div>'
   });
-}); 
